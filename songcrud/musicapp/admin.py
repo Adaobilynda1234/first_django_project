f2ff529d1a19cf92274534e31b1fcebb6a87aa63
Adaobilynda1234 <adaobilynda1234@gmail.com>
2022-10-28 21:45:12 +0100

    week 5 updated

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import song, Artiste, Lyric
 
 # Register your models here.
+admin.site.register(song)
+admin.site.register(Artiste)
+admin.site.register(Lyric)
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,7 +1,3 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse("Hello, this is my django app")
-
-# Create your views here.
+# Create your views
